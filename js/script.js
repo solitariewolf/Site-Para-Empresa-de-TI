2bd0cc92ac6f800294160a4d88f4bbfb480954c2
solitariewolf <watanasio@gmail.com>
2024-01-06 13:31:21 -0300

    Changes to be committed: 	modified:   css/style.css 	new file:   img/1.png 	new file:   img/2.png 	new file:   img/3.png 	deleted:    img/image1.png 	modified:   index.html 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -8,4 +8,27 @@ $(document).ready(function () {
       $("html, body").animate({scrollTop: 0}, 800);
     });
   });
-  
+
+// hamburguer
+
+$(".hamburger").click(function() {
+  $(this).toggleClass("aberto");
+  $(".menu ul").toggle();
+});
+
+// slider andar
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var slides = document.getElementsByClassName("slide");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(carousel, 3000); // Muda a imagem a cada 3 segundos
+}
